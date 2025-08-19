b3101d679923728dea1a3e20c7f5abd7c02790bd
samarth123 <gamarth234@gmail.com>
2025-08-19 11:53:48 +0530

    Add files via upload

---
+++ python .5.py
@@ -0,0 +1,16 @@
+Friends=int(input("enter the number of friends available for game:"))
+if friends ==2:
+    print ("selected game:chess")
+    print("lets us play chess")
+elif friends==4:
+    print ("selected game:carom")
+    print("let us play carom")
+elif friends==7:
+    print ("selected game:kabaddi")
+    print ("let us play kabaddi")
+elif friends==11:
+    print ("selected game:cricket")
+    print ("let us play cricket")
+else:
+    print("invalid number")
+          
