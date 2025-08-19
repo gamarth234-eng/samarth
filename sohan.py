746c0d6a8ff20635cada6c646902a65184705506
samarth123 <gamarth234@gmail.com>
2025-08-19 11:54:42 +0530

    Add files via upload

---
+++ sohan.py
@@ -0,0 +1,28 @@
+class node:
+	def _init_(self,data=node):
+		self.data=data
+		self.next=node
+	class linkedlist:
+            def _init_(self):
+                self.first=none
+            def insert(self,data):
+                temp=none(data)
+                if cur(self,data):
+                    while(cur.next):
+                        cur.next=next
+                        cur.next=temp
+                else:
+                    def iter(self,first):
+                        cur=(self.first)
+                    while cur:
+                        yields=cur.next
+                    cur.first=next
+            l1=linkedlist()
+            l1=insert(9)
+            l1=insert(98)
+            l1=insert("welcome")
+            l1=insert("govt polytechnic harohalli")
+            l1=insert(456,9)
+            l1=insert(545)
+            for x in (l1):
+                print(x)
